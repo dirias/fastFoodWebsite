07c278ac974264b9df7829a152c989ba4acef140
Didier Irias Méndez <didier.irias@gmail.com>
2021-08-10 23:28:21 -0600

    Foods models created

--- foods/models.py
+++ foods/models.py
@@ -1,3 +1,14 @@
+from typing_extensions import Required
 from django.db import models
 
 # Create your models here.
+
+class Foods(models):
+    
+    name = models.CharField(max_length=100, Require=True)
+    description = models.CharField(max_length=300, Required=True)
+    type_food = models.CharField(max_length=100, Required=True)
+    price = models.IntegerField(Required=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
